c8a729bf5d49c2cd4eda27861b1eee85253ab0eb
Bas Peters <bas@bprts.com>
2025-02-18 01:16:01 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,22 +9,22 @@ const PORT = process.env.PORT || 3000;
 let dbConnectionMessage = '';
 
 // Database connection
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'bprtscom_bprtscom',
-    password: 'V]6Z-J]Y,X9$',
-    database: 'admin'
-});
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'bprtscom_bprtscom',
+//     password: 'V]6Z-J]Y,X9$',
+//     database: 'admin'
+// });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        dbConnectionMessage = 'Error connecting to the database';
-        return;
-    }
-    console.log('Connected to the database');
-    dbConnectionMessage = 'Connected to the database';
-});
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to the database:', err);
+//         dbConnectionMessage = 'Error connecting to the database';
+//         return;
+//     }
+//     console.log('Connected to the database');
+//     dbConnectionMessage = 'Connected to the database';
+// });
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -32,9 +32,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('index', { dbConnectionMessage });
-});
+// app.get('/', (req, res) => {
+//     res.render('index', { dbConnectionMessage });
+// });
 
 setRoutes(app);
 
